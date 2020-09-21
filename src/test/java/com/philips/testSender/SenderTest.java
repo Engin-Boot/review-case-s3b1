8c860069c8c1ebbd1fe4d1705e35d08b11b4594f
RV313 <RV.13051995@gmail.com>
2020-09-22 00:12:14 +0530

    Updated src/test/java/com/philips/testSender/SenderTest.java

--- src/test/java/com/philips/testSender/SenderTest.java
+++ src/test/java/com/philips/testSender/SenderTest.java
@@ -3,16 +3,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.PrintStream;
-import java.net.URISyntaxException;
 import java.util.List;
 
 import org.junit.Test;
 
 import com.philips.sender.operation.CsvReader;
 import com.philips.sender.operation.PrintToConsole;
+import com.philips.sender.service.IPrintToConsole;
 
 public class SenderTest 
 {
@@ -20,8 +17,7 @@ public class SenderTest
 
 	@Test
 	public void checkFileEmpty() { 
-		String filename = "\\src\\test\\resources\\senderTestSample1.csv";
-		CsvReader csvr = new CsvReader(filename);
+		String filename = "src\\test\\resources\\senderTestSample1.csv";
 		 assertFalse(filename.isEmpty());
 	}
 	
@@ -29,42 +25,48 @@ public class SenderTest
 	
 	@Test
 	public void checkRowCount() {
-		String filepath = "\\src\\test\\resources\\senderTestSample2.csv";
+		String filepath = "src\\test\\resources\\senderTestSample2.csv";
 		CsvReader csvr = new CsvReader(filepath);
 		List<String[]> lineCount = csvr.readUsingBufferedReader();
-		assertEquals(lineCount.size(),7);
+		assertEquals(6,lineCount.size());
 		
 	}
 	
 
 	@Test
-	public void testDateIsNull() {
-		DateFormat df = new DateFormat();
-		assertFalse(df.isThisDateValid(null, "dd//mm/yyyy"));
+	public void checkHeaderCount() {
+		String filepath = "src\\test\\resources\\senderTestSample1.csv";
+		CsvReader csvr = new CsvReader(filepath);
+		List<String[]> lineCount = csvr.readUsingBufferedReader();
+		String columnName[] = lineCount.get(0);
+		PrintToConsole print = new PrintToConsole();
+		int index = print.getIndexForColumnName(columnName,"Comments");
+		assertEquals("ReviewDate",columnName[0]);
+		
+		assertEquals("Comments",columnName[1]);
+		
+		assertEquals(1,index);
+		
 	}
 	
 	
-	@Test
-	public void testDateFormatIsInvalid() {
-		DateFormat df = new DateFormat();
-		assertFalse(df.isThisDateValid("2012/02/20", "dd/MM/yyyy"));
-	}
 	
-	@Test
+	/*@Test
 	public void printTest() throws Exception {
 	      ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	      System.setOut(new PrintStream(outContent));
-	      String filename = "\\src\\test\\resources\\senderTestSample1.csv";
+	      
+	      String filename = "src\\test\\resources\\senderTestSample1.csv";
 	      CsvReader csvr = new CsvReader(filename);
 	      List<String[]> wordList = csvr.readUsingBufferedReader();
 	      PrintToConsole printing = new PrintToConsole();
 	      printing.printToConsole(wordList, 1);
 	      
-	      String expectedOutput  = "comments\nwhat does this help with?\nchange spelling"; 
+	      String expectedOutput  = "Comments\nwhat does this help with?\nchange spelling"; 
 
-	     
+	      String line = outContent.
 	     assertEquals(expectedOutput, outContent.toString());
-	}
+	}*/
 	
 	
 }
